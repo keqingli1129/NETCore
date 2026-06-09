368c6be66e9dddf50d0ba5b1c3f734543c663140
Keqing Li <keqingli1129@gmail.com>
2026-06-09 15:53:41 -0500

    remove not used projects

--- UserRoles/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UserRoles.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- UserRoles/Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace UserRoles.Models
-{
-    public class Users : IdentityUser
-    {
-        public string FullName { get; set; } = "";
-    }
-}
